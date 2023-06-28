73a768086ffd05b9bc7268da72ab87685c742a31
Andy Cockroft <acockroft1994@gmail.com>
2023-06-28 13:24:04 +0200

    validation complete

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User.model");
+const mongoose = require("mongoose");
 
 const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
@@ -15,6 +16,23 @@ router.post("/signup", (req, res, next) => {
   //console.log("The form data ", req.body);
   const { username, email, password } = req.body;
 
+  // make sure users fill all mandatory fields:
+  if (!username || !email || !password) {
+    res.render("auth/signup", {
+      errorMessage:
+        "All fields are mandatory. Please provide your username, email and password.",
+    });
+    return;
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(500).render("auth/signup", {
+      errorMessage:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+    });
+    return;
+  }
   bcryptjs
     .genSalt(saltRounds)
     .then((salt) => bcryptjs.hash(password, salt))
@@ -33,7 +51,22 @@ router.post("/signup", (req, res, next) => {
       // console.log('Newly created user is: ', userFromDB);
       res.redirect("/userProfile");
     })
-    .catch((error) => next(error));
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render("auth/signup", { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        console.log(
+          " Username and email need to be unique. Either username or email is already used. "
+        );
+
+        res.status(500).render("auth/signup", {
+          errorMessage:
+            "Username/email already in use and/or incorrect password.",
+        });
+      } else {
+        next(error);
+      }
+    });
 });
 
 //GET INFO TO DISPLAY USER PROFILE
